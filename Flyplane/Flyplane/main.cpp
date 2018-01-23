d7168ca730522daa4227efd9789daaf0653d148b
Tristan Axelsson Smith <tristan23@live.se>
2018-01-23 10:04:52 +0100

    added engine states with a test state

--- Flyplane/Flyplane/main.cpp
+++ Flyplane/Flyplane/main.cpp
@@ -1,13 +1,27 @@
 #include <GL\glew.h>
-
 #include "window.h"
+#include "enginestate.h"
 
 int main(void)
 {
 	Window w;
 	w.open(1280, 720);
 
+
+	EngineState* engine = new TestState();
+	engine->init();
+
 	while (!w.shouldClose()) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		engine->update();
+
+		EngineState* new_state = engine->newState();
+		if (new_state) {
+			delete engine;
+			engine = new_state;
+			engine->init();
+		}
 
 		w.update();
 	}
