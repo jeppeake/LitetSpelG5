d07093aac3c9f09aa8ad639111927afb0f8a56bd
FelixNilsson <felixnilsson100@gmail.com>
2018-01-24 13:59:39 +0100

    updated renderer

--- Flyplane/Flyplane/renderer.cpp
+++ Flyplane/Flyplane/renderer.cpp
@@ -19,11 +19,11 @@ void Renderer::Render(Model &model) {
 	this->shader.use();
 	shader.uniform("texSampler", 0);
 	model.texture.bind(0);
-	this->tal += 0.0005;
+	this->tal += 0.005;
 	this->shader.uniform("ViewProjMatrix", this->camera.getProjMatrix() * this->camera.getViewMatrix());
 	for (int i = 0; i < model.model_meshes.size(); i++) {
 		model.model_meshes[i].first->bind();
-		this->shader.uniform("modelMatrix", glm::rotate(model.model_meshes[i].second, this->tal, glm::vec3(0, 1, 0)));
+		this->shader.uniform("modelMatrix", glm::rotate(model.model_meshes[i].second, this->tal, glm::vec3(1, 0, 0)));
 		glDrawElements(GL_TRIANGLES, model.model_meshes[i].first->numIndices(), GL_UNSIGNED_INT, 0);
 	}
 }
