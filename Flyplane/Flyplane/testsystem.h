a35b3fd46b87a5bb065f8311a39d00119bd2a885
kalasnokov <gustaffagerstrom@gmail.com>
2018-01-24 12:11:25 +0100

    Gravity

--- Flyplane/Flyplane/testsystem.h
+++ Flyplane/Flyplane/testsystem.h
@@ -3,6 +3,7 @@
 #include <entityx\entityx.h>
 #include <entityx\Entity.h>
 #include "transform.h"
+#include "physics.h"
 #include <iostream>
 
 using namespace entityx;
@@ -11,13 +12,16 @@ struct TestSystem : public entityx::System<TestSystem> {
 
 	void update(entityx::EntityManager &es, entityx::EventManager &events, TimeDelta dt) override {
 
-
+		ComponentHandle<Physics> physics;
 		ComponentHandle<Transform> transform;
-		for (Entity entity : es.entities_with_components(transform)) {
+		for (Entity entity : es.entities_with_components(physics, transform)) {
+			physics = entity.component<Physics>();
 			transform = entity.component<Transform>();
-			transform->pos += glm::vec3(0.1);
 
-			//std::cout << transform->pos.x << "\n";
+			physics->calcVelocity();
+			physics->calcAcceleration();
+			
+			transform->pos += float(dt) * physics->velocity;
 		}
 	};
 };
