6ad4246389b2cd80aef8817947ab978e84038964
FelixNilsson <felixnilsson100@gmail.com>
2018-01-25 14:22:07 +0100

    updated shadow shaders

--- Flyplane/Flyplane/terrainFragmentShader.glsl
+++ Flyplane/Flyplane/terrainFragmentShader.glsl
@@ -1,15 +1,24 @@
 uniform sampler2D texSampler;
-in vec3 vPos;
-in vec3 vNormal;
-in vec2 vTex;
+uniform sampler2D shadowMap;
+uniform mat4 shadowMatrix;
+varying vec3 Pos;
+varying vec3 Normal;
+varying vec2 Tex;
 
 
 void main() {
+	vec3 shadowCoord = (shadowMatrix * vec4(Pos, 1)).xyz;
+	float depth = texture(shadowMap, shadowCoord.xy).z;
+	float visibility = 1.0;
+	if(depth < shadowCoord.z) {
+		visibility = 0.5;
+	}
+
 	vec3 sun = vec3(0, 1, 1);
 	normalize(sun);
-	normalize(vNormal);
-	float result = dot(sun, vNormal);
+	normalize(Normal);
+	float result = dot(sun, Normal);
 	result = clamp(result, 0, 1);
-	vec3 color = texture(texSampler, vec2(vTex.x, 1 - vTex.y)).rgb;
-	gl_FragColor = vec4(color * result, 1) + vec4(color * 0.2, 1);
+	vec3 color = texture(texSampler, vec2(Tex.x, 1 - Tex.y)).rgb;
+	gl_FragColor = vec4(color * result, 1) * visibility + vec4(color * 0.2, 1);
 }
