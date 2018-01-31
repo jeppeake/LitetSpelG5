edf4ce7f278ff462df97d05a3bb63c7d334328a4
Tristan Axelsson Smith <tristan23@live.se>
2018-01-31 14:55:24 +0100

    added transform to BB

--- Flyplane/Flyplane/boundingbox.h
+++ Flyplane/Flyplane/boundingbox.h
@@ -1,7 +1,15 @@
 #pragma once
+
+#include "transform.h"
+
 struct BoundingBox {
 	glm::vec3 sides[3];
 	glm::vec3 center;
+	
+	Transform transform;
+
 	bool intersect(BoundingBox &box);
 	bool intersect(const glm::vec3& point);
+
+	void setTransform(const Transform& transform);
 };
