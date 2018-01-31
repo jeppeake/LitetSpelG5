5ce29ff79362b54cc7169b183c11738cfbe647b7
Tristan Axelsson Smith <tristan23@live.se>
2018-01-31 14:34:03 +0100

    obb point intersection

--- Flyplane/Flyplane/boundingbox.cpp
+++ Flyplane/Flyplane/boundingbox.cpp
@@ -10,13 +10,19 @@ void SATtest(const glm::vec3 &axis, const glm::vec3 *corners, float& minAlong, f
 		if (dotval > maxAlong) maxAlong = dotval;
 	}
 }
+
+
 bool isBetweenOrdered(float val, float lowerBound, float upperBound) {
 	return lowerBound <= val && val <= upperBound;
 }
+
+
 bool overlaps(float min1, float max1, float min2, float max2)
 {
 	return isBetweenOrdered(min2, min1, max1) || isBetweenOrdered(min1, min2, max2);
 }
+
+
 bool BoundingBox::intersect(BoundingBox & box)
 {
 	glm::vec3 normals[3];
@@ -69,6 +75,39 @@ bool BoundingBox::intersect(BoundingBox & box)
 		SATtest(bnormals[i], corners, shape1Min, shape1Max);
 		SATtest(bnormals[i], bcorners, shape2Min, shape2Max);
 		if (!overlaps(shape1Min, shape1Max, shape2Min, shape2Max))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+
+bool BoundingBox::intersect(const glm::vec3 & point)
+{
+	glm::vec3 normals[3];
+	glm::vec3 corners[8];
+	normals[0] = glm::normalize(sides[0]);
+	normals[1] = glm::normalize(sides[1]);
+	normals[2] = glm::normalize(sides[2]);
+	int i = 0;
+	for (float x = -1; x <= 1; x += 2)
+	{
+		for (float y = -1; y <= 1; y += 2)
+		{
+			for (float z = -1; z <= 1; z += 2)
+			{
+				corners[i] = x * sides[0] + y * sides[1] + z * sides[2] + center;
+				i++;
+			}
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		float min, max;
+		SATtest(normals[i], corners, min, max);
+		float pdot = dot(normals[i], point);
+		if (pdot <= min && pdot >= max)
 		{
 			return false;
 		}
--- Flyplane/Flyplane/boundingbox.h
+++ Flyplane/Flyplane/boundingbox.h
@@ -3,5 +3,5 @@ struct BoundingBox {
 	glm::vec3 sides[3];
 	glm::vec3 center;
 	bool intersect(BoundingBox &box);
-
+	bool intersect(const glm::vec3& point);
 };
