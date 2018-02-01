660efa3fe368520f1aa82f406dbdb0f376c8b00c
Jesper Åkesson <akesson.jesper@hotmail.com>
2018-02-01 15:11:55 +0100

    new map

--- Flyplane/Flyplane/playingstate.cpp
+++ Flyplane/Flyplane/playingstate.cpp
@@ -145,7 +145,7 @@ void PlayingState::init()
 	weapons.emplace_back(bomb, &weaponmodel, &missile, glm::vec3(0, -0.3, -0.1));
 
 
-	hm = new Heightmap("assets/textures/racesmall.png", "assets/textures/bog.png");
+	hm = new Heightmap("assets/textures/slojp.png", "assets/textures/bog.png");
 	hm->pos.x -= 2560;
 	hm->pos.z -= 2560;
 	entity.assign <Equipment>(pweapons, weapons);
