b1e892ddfd4772c454df867681de8624c00f13e3
JesperNilsson11 <jene15@student.bth.se>
2018-02-01 15:34:36 +0100

    updated sound

--- Flyplane/Flyplane/soundsystem.h
+++ Flyplane/Flyplane/soundsystem.h
@@ -18,22 +18,38 @@ struct SoundSystem : public System<SoundSystem> {
 			sound = entity.component<SoundComponent>();
 			transform = entity.component<Transform>();
 
-			SoundComponent s = *sound.get();
+			SoundComponent* s = sound.get();
 			Transform t = *transform.get();
 
-			s.sound.setPosition(t.pos.x, t.pos.y, t.pos.z);
+			s->sound.setPosition(t.pos.x, t.pos.y, t.pos.z);
+		}
+
+		ComponentHandle<BurstSoundComponent> burstSound;
+		for (Entity entity : es.entities_with_components(burstSound, transform)) {
+			burstSound = entity.component<BurstSoundComponent>();
+			transform = entity.component<Transform>();
+
+			BurstSoundComponent* s = burstSound.get();
+			Transform* t = transform.get();
+
+			s->sound.setPosition(t->pos.x, t->pos.y, t->pos.z);
+			if (burstSound && (Input::isKeyDown(GLFW_KEY_LEFT_CONTROL) || Input::isMouseButtonDown(GLFW_MOUSE_BUTTON_LEFT) || Input::gamepad_button_pressed(GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER))) {
+				//s->sound.stop();
+				if (s->sound.getStatus() != s->sound.Playing)
+					s->sound.play();
+			}
 		}
 		
 		ComponentHandle<PlayerComponent> player;
-		for (Entity entity : es.entities_with_components(player, sound, transform)) {
-			sound = entity.component<SoundComponent>();
+		for (Entity entity : es.entities_with_components(player, transform)) {
+			//sound = entity.component<SoundComponent>();
 			transform = entity.component<Transform>();
 
-			SoundComponent s = *sound.get();
+			//SoundComponent s = *sound.get();
 			Transform t = *transform.get();
 
-			s.sound.setPosition(t.pos.x, t.pos.y, t.pos.z);
-			//sf::Listener::setPosition(t.pos.x, t.pos.y, t.pos.z);
+			//s.sound.setPosition(t.pos.x, t.pos.y, t.pos.z);
+			sf::Listener::setPosition(t.pos.x, t.pos.y, t.pos.z);
 			glm::vec3 vec = glm::toMat3(t.orientation) * glm::vec3(0.0, 0.0, 1.0);
 			sf::Listener::setDirection(vec.x, vec.y, vec.z);
 			vec = glm::toMat3(t.orientation) * glm::vec3(0.0, 1.0, 0.0);
