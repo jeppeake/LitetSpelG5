992f5828dcab978915ba9fe9d81ee2d525ebdb62
FelixNilsson <felixnilsson100@gmail.com>
2018-02-01 10:07:48 +0100

    updated shaders

--- Flyplane/Flyplane/fragmentShader.glsl
+++ Flyplane/Flyplane/fragmentShader.glsl
@@ -11,7 +11,7 @@ void main() {
 	vec3 shadowCoord = (shadowMatrix * vec4(Pos, 1)).xyz;
 	float depth = texture(shadowMap, shadowCoord.xy).r;
 	float visibility = 1.0;
-	if(depth < shadowCoord.z - 0.00001) {
+	if(depth < shadowCoord.z - 0.001) {
 		visibility = 0.0;
 	}
 
