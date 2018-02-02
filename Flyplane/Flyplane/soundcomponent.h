64eeb6cc1cccad8b733885955b707f69ec859436
JesperNilsson11 <jene15@student.bth.se>
2018-02-02 09:52:16 +0100

    updated sound

--- Flyplane/Flyplane/soundcomponent.h
+++ Flyplane/Flyplane/soundcomponent.h
@@ -17,7 +17,7 @@ struct BurstSoundComponent {
 	BurstSoundComponent(sf::SoundBuffer& buffer) {
 		sound.setBuffer(buffer);
 		//sound.play();
-		sound.setLoop(false);
+		sound.setLoop(true);
 		sound.setMinDistance(100);
 		sound.setAttenuation(1);
 	}
