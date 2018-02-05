80160dd0b753d1ee409dc3a217df25ff410c4442
Jesper Åkesson <akesson.jesper@hotmail.com>
2018-02-05 15:15:25 +0100

    Added asset loader, no more global variables in playingstate! Can load models, heightmaps, sounds. Assets are retreived with an assigned name.

--- Flyplane/Flyplane/soundbuffers.h
+++ Flyplane/Flyplane/soundbuffers.h
@@ -2,7 +2,7 @@
 
 #include <SFML/Audio.hpp>
 
-extern sf::SoundBuffer flyingSB;
+/*extern sf::SoundBuffer flyingSB;
 extern sf::SoundBuffer missileSB;
 extern sf::SoundBuffer bulletSB;
-extern sf::SoundBuffer machinegunSB;
+extern sf::SoundBuffer machinegunSB;*/
