2795b744a83cf7de14ddd7e85d9ddbb20688b63e
JesperNilsson11 <jene15@student.bth.se>
2018-02-06 10:21:22 +0100

    updated menu

--- Flyplane/Flyplane/menustate.cpp
+++ Flyplane/Flyplane/menustate.cpp
@@ -1,6 +1,7 @@
 #include "menustate.h"
 #include "input.h"
 #include "playingstate.h"
+#include "assetloader.h"
 
 MenuState::~MenuState() {
 }
@@ -11,6 +12,8 @@ void MenuState::init() {
 void MenuState::update(double dt) {
 	glClearColor(1, 1, 1, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	AssetLoader::getLoader().getText()->drawText("Press enter to start!", glm::vec2(400, 500), glm::vec3(1, 0, 0), 1);
 
 	if (Input::isKeyPressed(GLFW_KEY_ENTER)) {
 		this->changeState(new PlayingState());
