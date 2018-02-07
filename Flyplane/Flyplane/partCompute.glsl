de8fb2c7e8a7f9d5145f9f2ac7fa1496b209ad6d
Patrik Sjölund <patrik__96@hotmail.com>
2018-02-07 19:10:35 +0100

    Progress?

--- Flyplane/Flyplane/partCompute.glsl
+++ Flyplane/Flyplane/partCompute.glsl
@@ -23,5 +23,10 @@ layout(local_size_x = 128, local_size_y = 1, local_size_z = 1) in;
 void main()
 {
 	uint gid = gl_GlobalInvocationID.x;
-	Positions[gid] = vec4(spawn, 1.0);
+	Positions[gid] = vec4(spawn + direction * Lives[gid], 1.0);
+	Lives[gid] += dt;
+	if(Lives[gid] >= life)
+	{
+		Lives[gid] = 0;
+	}
 }
