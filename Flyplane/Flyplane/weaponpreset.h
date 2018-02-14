71463367e0047de1c6dda3c5e343d090bb352ac5
Jesper Åkesson <akesson.jesper@hotmail.com>
2018-02-14 11:17:57 +0100

    Configed explosions

--- Flyplane/Flyplane/weaponpreset.h
+++ Flyplane/Flyplane/weaponpreset.h
@@ -12,6 +12,7 @@ public:
 	unsigned int ammo, lifetime;
 	double speed, mass, cooldown, turnRate, scale, projScale;
 	bool infAmmo, isMissile, dissappear;
+	double detonateRange, explodeRadius, explodeDamage;
 	glm::vec3 extraOffset = glm::vec3(0,0,0);
 	WeaponPreset() {}
 	~WeaponPreset() {}
