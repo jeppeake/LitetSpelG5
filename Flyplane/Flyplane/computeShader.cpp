00d1f91c1daa34307d7074e41b5c5a30c44ef9f7
Patrik Sjölund <patrik__96@hotmail.com>
2018-02-14 18:23:00 +0100

    Added computeShader class

---
+++ Flyplane/Flyplane/computeShader.cpp
@@ -0,0 +1,124 @@
+#include "computeShader.h"
+bool computeShader::create(const std::string & path)
+{
+	std::ifstream file(path);
+	std::string c = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	const char *code = c.c_str();
+	const GLuint shader = glCreateShader(GL_COMPUTE_SHADER);
+	if (shader) {
+		glShaderSource(shader, 1, &code, NULL);
+		glCompileShader(shader);
+		const GLuint program = glCreateProgram();
+		if (program) {
+			GLint compiled = GL_FALSE;
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+			glProgramParameteri(program, GL_PROGRAM_SEPARABLE, GL_TRUE);
+			if (compiled) {
+				glAttachShader(program, shader);
+				glLinkProgram(program);
+				glDetachShader(program, shader);
+			}
+			else
+			{
+				GLint success = 0;
+				glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+				if (success == GL_FALSE)
+				{
+					GLint log_size = 0;
+					glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_size);
+					std::vector<GLchar> error(log_size);
+					glGetShaderInfoLog(shader, log_size, &log_size, &error[0]);
+					std::string errorstr{ &error[0] };
+
+					std::cout << "Error in ComputeShader:\n" << errorstr << "\n";
+
+					glDeleteShader(shader);
+					system("pause");
+					return false;
+				}
+			}
+		}
+		glDeleteShader(shader);
+		this->program = program;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+GLuint computeShader::findUniformLocation(const std::string & name)
+{
+	auto it = uniform_locations.find(name);
+	GLuint uniform_location;
+	if (it == uniform_locations.end())
+	{
+		uniform_location = glGetUniformLocation(program, name.c_str());
+		if (uniform_location == -1) {
+			std::cout << "ERROR: could not find '" << name << "' in shader\n";
+		}
+		uniform_locations[name] = uniform_location;
+	}
+	else
+		uniform_location = it->second;
+	return uniform_location;
+}
+void computeShader::use()
+{
+	glUseProgram(program);
+}
+void computeShader::uniformv(const std::string & name, GLuint count, const glm::mat4* matrices)
+{
+	glUniformMatrix4fv(findUniformLocation(name), count, GL_FALSE, (GLfloat*)matrices);
+}
+void computeShader::uniform(const std::string & name, const glm::mat4& matrix)
+{
+	uniformv(name, 1, &matrix);
+}
+
+
+void computeShader::uniformv(const std::string & name, GLuint count, const GLfloat* values)
+{
+	glUniform1fv(findUniformLocation(name), count, (GLfloat*)values);
+}
+void computeShader::uniform(const std::string & name, GLfloat value)
+{
+	uniformv(name, 1, &value);
+}
+
+
+void computeShader::uniformv(const std::string & name, GLuint count, const glm::vec2* vectors)
+{
+	glUniform2fv(findUniformLocation(name), count, (GLfloat*)vectors);
+}
+void computeShader::uniform(const std::string & name, const glm::vec2& vector)
+{
+	uniformv(name, 1, &vector);
+}
+
+
+void computeShader::uniformv(const std::string & name, GLuint count, const glm::vec3* vectors)
+{
+	glUniform3fv(findUniformLocation(name), count, (GLfloat*)vectors);
+}
+void computeShader::uniform(const std::string & name, const glm::vec3& vector)
+{
+	uniformv(name, 1, &vector);
+}
+
+void computeShader::uniformv(const std::string & name, GLuint count, const glm::vec4* vectors)
+{
+	glUniform4fv(findUniformLocation(name), count, (GLfloat*)vectors);
+}
+void computeShader::uniform(const std::string & name, const glm::vec4& vector)
+{
+	uniformv(name, 1, &vector);
+}
+
+void computeShader::uniformv(const std::string & name, GLuint count, const GLint* values)
+{
+	glUniform1iv(findUniformLocation(name), count, values);
+}
+void computeShader::uniform(const std::string & name, GLint value)
+{
+	uniformv(name, 1, &value);
+}
---
+++ Flyplane/Flyplane/computeShader.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <GL/glew.h>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <map>
+#include <glm/glm.hpp>
+class computeShader
+{
+private:
+	GLuint program;
+	std::map<std::string, GLuint> uniform_locations;
+	GLuint findUniformLocation(const std::string& name);
+public:
+	bool create(const std::string &path);
+	inline GLuint getProgram()
+	{
+		return program;
+	}
+	void use();
+	void uniformv(const std::string& name, GLuint count, const glm::mat4* matrices);
+	void uniform(const std::string& name, const glm::mat4& matrix);
+
+	void uniformv(const std::string& name, GLuint count, const GLfloat* values);
+	void uniform(const std::string& name, const GLfloat value);
+
+	void uniformv(const std::string& name, GLuint count, const glm::vec2* vectors);
+	void uniform(const std::string& name, const glm::vec2& vector);
+
+	void uniformv(const std::string& name, GLuint count, const glm::vec3* vectors);
+	void uniform(const std::string& name, const glm::vec3& vector);
+
+	void uniformv(const std::string& name, GLuint count, const glm::vec4* vectors);
+	void uniform(const std::string& name, const glm::vec4& vector);
+
+	void uniformv(const std::string& name, GLuint count, const GLint* values);
+	void uniform(const std::string& name, const GLint value);
+};
