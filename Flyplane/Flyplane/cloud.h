b3f278bd87643fbec12fbe28e7decce1b009b347
FelixNilsson <felixnilsson100@gmail.com>
2018-02-14 14:39:15 +0100

    updated cloud

--- Flyplane/Flyplane/cloud.h
+++ Flyplane/Flyplane/cloud.h
@@ -10,6 +10,6 @@ struct Cloud {
 	std::vector<glm::mat4> transform;
 	Cloud() {
 		texture.loadTexture("assets/textures/cloud.png", 1);
-		transform.push_back(glm::translate(glm::vec3(0, 10000, 0)) * ) * glm::scale(glm::vec3(500, 300, 1)));
+		transform.push_back(glm::translate(glm::vec3(0, 10000, 0)) * glm::rotate(3.14f/2.0f, glm::vec3(1, 0, 0)) * glm::scale(glm::vec3(500, 300, 1)));
 	}
 };
